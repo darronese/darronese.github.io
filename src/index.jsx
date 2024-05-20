8579034c085aa5da904df2239faa6dee6000e6fd
darronese <darron198@hotmail.com>
2024-05-19 21:53:40 -0700

    added a bar that links to every page

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import SideBar from './sidebar.jsx';
+import Footer from './sidebar.jsx';
 
-const App = () => {
-  return (
-    <div>
-      <SideBar />
-      <header className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg"></header>
-    </div>
-  );
-};
+
+const App = () => (
+  <div>
+    <Footer />
+  </div>
+);
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
+import { FaUser, FaEnvelope, FaFileAlt, FaProjectDiagram, FaHome } from 'react-icons/fa';
 
-const SideBar = () => {
-  return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-      <h2>Sidebar</h2>
-      <ul>
-        <li><a href="about.html">About</a></li>
-        <li><a href="contacts.html">Contacts</a></li>
-        <li><a href="resume.html">Resume</a></li>
-        <li><a href="projects.html">Projects</a></li>
-      </ul>
-    </div>
-  );
-};
+const SideBarIcon = ({ icon, text }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
+  </div>
+);
 
-export default SideBar;
+const SideBar = () => (
+  <div className="sidebarFormat">
+    <ul className="flex">
+      <li>
+        <a href="index.html" title="Goes to home page">
+          <SideBarIcon icon={<FaHome size="28" />} text="Home" />
+        </a>
+      </li>
+      <li>
+        <a href="about.html" title="Goes to about page">
+          <SideBarIcon icon={<FaUser size="28" />} text="About" />
+        </a>
+      </li>
+      <li>
+        <a href="contacts.html" title="Goes to contacts page">
+          <SideBarIcon icon={<FaEnvelope size="28" />} text="Contacts" />
+        </a>
+      </li>
+      <li>
+        <a href="resume.html">
+          <SideBarIcon icon={<FaFileAlt size="28" />} text="Resume" />
+        </a>
+      </li>
+      <li>
+        <a href="projects.html">
+          <SideBarIcon icon={<FaProjectDiagram size="28" />} text="Projects" />
+        </a>
+      </li>
+    </ul>
+  </div>
+);
+
+const Footer = () => (
+  <div className="sidebarFormat">
+    <SideBar />
+  </div>
+);
+
+export default Footer;
