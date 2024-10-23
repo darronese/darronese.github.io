df70f2b015c8070165f9ea6e5a8f7d4619ea2b56
darronese <darron198@hotmail.com>
2024-10-23 11:51:52 -0700

    changed styles

--- src/about.jsx
+++ src/about.jsx
@@ -6,10 +6,17 @@ import './style.css';
 const About = () => (
   <div className="background">
     <h1 className="aboutHeading">About</h1>
-      <p className="aboutDescription">
+      <p className="personalDescription">
       My name is Darron Li and I am an aspiring software engineer. I am currently
       studying at Washington State University, pursuing a bachelor's in computer science.
       </p>
+    <h2 className="skillsHeading">Skills</h2>
+      <ul className="skillsList">
+        <li>Proficient in C/C++, Python, Javascript, Html, CSS</li>
+        <li>Experience with modern front-end libraries like React and TailwindCSS</li>
+        <li>Knowledge of software devlopment tools such as Github, Webpack and source control (Git)</li>
+        <li>Proficient in understanding of algorithms, data structures, and object-oriented programming</li>
+      </ul>
   </div>
 );
 
