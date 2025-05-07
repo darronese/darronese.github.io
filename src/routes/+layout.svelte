0dfa21680e24e6c6ebe22cbe86f623ae6e596d55
darron <darron198@hotmail.com>
2025-05-07 17:12:59 +0000

    added some testing fa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <script>
   import "../app.css";
   import Fa from 'svelte-fa'
-  import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons/index.es'
+  import { faFlag } from '@fortawesome/free-solid-svg-icons'
   let { children } = $props();
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
 8 columns for tablet
 4 columns for mobile
 -->
+<script>
+import Fa from "svelte-fa";
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+</script>
 
 <div class="flex flex-col sm:flex-row items-center gap-y-6">
   <div class="basis-auto sm:basis-2/3 p-6 text-center">
@@ -11,6 +17,9 @@
       <dt class="text-[#90aead] italic"> aspring software engineer </dt>
       <dt class = "text-[#90aead] italic"> self proclaimed professional musician </dt>
     </dl>
+    <Fa icon={faGithub}/>
+    <Fa icon={faLinkedin}/>
+    <Fa icon={faEnvelope}/>
   </div>
   <div class="object-contain sm:basis-1/4 p-6 sm:pt-30">
     <img class="rounded-2xl" src="/temp.png"/>
