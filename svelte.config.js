d9e1565fdfa7af2b9741923e004b6168906fb1ce
darronese <darron198@hotmail.com>
2025-05-14 06:24:14 +0800

    chaged svelte.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,8 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
-    kit: {
-      adapter: adapter(),
-      paths: {
-        base:
-          process.env.NODE_ENV === "production" ? "/darronese.github.io" : "",
-      },
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/darronese.github.io" : "",
     },
   },
 };
