e70c6283cdf87ba1f0e48124c04cf17431b3df14
darronese <darron198@hotmail.com>
2025-05-15 16:39:06 +0800

    added to about me

--- src/lib/about.svelte
+++ src/lib/about.svelte
@@ -1,10 +1,21 @@
+<script lang="ts">
+</script>
+
 <!-- PAGES -->
 <div class="flex flex-col sm:flex-row items-center gap-y-6">
   <div class="basis-auto sm:basis-2/3 p-6 text-center">
     <h1 class="font-bold text-7xl text-[#E5DCC5] p-4 sm:pt-30">About</h1>
-    <dl class="font-normal text-[#848fa5]">
-      <dt class="italic"> aspring software engineer </dt>
-      <dt class = "italic"> self proclaimed professional musician </dt>
-    </dl>
+    <p class="italic">
+      "There are no men like me. There's only me."
+    </p>
+    <p class="">
+      - Jaime Lannister (ASOIAF)
+    </p>
   </div>
+  <p class="object-contain sm:basis-1/3 sm:w-30 p-4 sm:pt-60 text-center">
+    Hi there! My name is Darron and I am an aspiring software engineering with a passion for music. While impatiently waiting for <span class="underline italic decoration-[#c14153]"> Winds of Winter </span> to come out, I have decided to delve into the more technological side of me and explore the different worlds of software engineering. I love learning about new technlogies, even if they seem to bore me (<span class="underline decoration-[#03fc43]">cybersecurity</span>).
+  </p>
 </div>
+  <div class="basis-auto sm:basis-2/3 p-6 text-center">
+    <h2 class="font-bold text-4xl text-[#E5DCC5] p-4 sm:pt-30"> Skills </h2>
+  </div>
--- src/lib/projects.svelte
+++ src/lib/projects.svelte
@@ -35,8 +35,13 @@ onMount(() => {
 <div class="flex flex-col sm:flex-row items-center gap-y-6">
   <div class="basis-auto sm:basis-2/3 p-6">
     <h1 class="title font-bold text-7xl text-[#E5DCC5] p-4 sm:pt-30">Projects</h1>
+    <p class="text-center">
+      WIP
+    </p>
+    <!--
     <div class="object-contain">
       <img class="project rounded-lg" src="/home.png" alt="Game Home Screen"/>
     </div>
+    -->
   </div>
 </div>
