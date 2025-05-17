71e438e2c53e935e6b6cabea7808a57e25044c7a
darronese <darron198@hotmail.com>
2025-05-17 15:42:00 +0800

    removed git ignore from wasm

---
+++ src/lib/embed/pkg/wasm_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_postscriptinterpreter_free: (a: number, b: number) => void;
+export const postscriptinterpreter_new: () => number;
+export const postscriptinterpreter_run_interpreter: (a: number, b: number, c: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
