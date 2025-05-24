08721619404ef47c4253396f1b7db172e79374cd
darronese <darron198@hotmail.com>
2025-05-24 11:57:04 +0800

    changed name of one page

--- src/routes/agathon/+page.svelte
+++ src/routes/agathon/+page.svelte
@@ -52,7 +52,7 @@ onMount(async () => {
   </div>
   <div class="h-screen flex flex-col sm:flex-row items-center gap-y-6">
     <div class="basis-auto sm:basis-2/3 p-6 text-center">
-      <h1 class="font-bold text-7xl text-[#E5DCC5] pt-12 sm:pt-4">PostScript Interpreter</h1>
+      <h1 class="font-bold text-7xl text-[#E5DCC5] pt-12 sm:pt-4">AgATHON</h1>
       <p class="text-balance pt-12">
         WIP
       </p>
